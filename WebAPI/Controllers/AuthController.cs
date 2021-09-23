03739b3f74e0a3977f46dae418e7834705381015
Enes Fındık <enesfindik.34@gmail.com>
2021-09-23 15:17:37 +0300

    21 login, register fix.

--- WebAPI/Controllers/AuthController.cs
+++ WebAPI/Controllers/AuthController.cs
@@ -30,7 +30,7 @@ namespace WebAPI.Controllers
             var result = _authManager.CreateAccessToken(userToLogin.Data);
             if (result.Success)
             {
-                return Ok(result.Data);
+                return Ok(result);
             }
             return BadRequest(result.Message);
         }
@@ -47,7 +47,7 @@ namespace WebAPI.Controllers
             var result = _authManager.CreateAccessToken(registerResult.Data);
             if (result.Success)
             {
-                return Ok(result.Data);
+                return Ok(result);
             }
 
             return BadRequest(result.Message);
